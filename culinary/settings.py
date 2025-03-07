32dca7c66d432ba1b941401df62834f782c3db74
David <ashuaib1991@gmail.com>
2025-03-07 16:36:20 +0100

    Fix ALLOWED_HOSTS with correct domain

--- culinary/settings.py
+++ culinary/settings.py
@@ -34,10 +34,18 @@ SECRET_KEY = 'django-insecure-on3uso5!_yug6toip)n4die+l69j^q+$jjgcbx3ziyfmv1tkm@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = ["wazobiaculinary.com ", "www.wazobiaculinary.com ", "wazobia-culinary-1.onrender.com"]
+
 
 import os
+
+ALLOWED_HOSTS = [
+    "www.wazobiaculinary.com",
+    "wazobiaculinary.com",
+    "wazobia-culinary-1.onrender.com",
+]
+
+print(f"🔍 DEBUG: ALLOWED_HOSTS = {ALLOWED_HOSTS}")  # Check Render logs for this output
+
 
 
 
