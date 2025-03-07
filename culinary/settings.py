2506220b8451ec8a2f15ae159149e39012169578
David <ashuaib1991@gmail.com>
2025-03-07 13:56:34 +0100

    gunicorn added

--- culinary/settings.py
+++ culinary/settings.py
@@ -90,12 +90,21 @@ WSGI_APPLICATION = 'culinary.wsgi.application'
 #Database
 #https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
 
 
 
